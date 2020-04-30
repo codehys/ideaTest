63f9cd39f8f0d43c2dae900fb758cb7ec330de26
sen <1224124407@qq.com>
2020-04-30 22:22:39 +0800

    idea的git使用

---
+++ src/HelloGit.java
@@ -0,0 +1,10 @@
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+    }
+}
